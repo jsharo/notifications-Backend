287661c1cd827360d8ae4ddd13ab408c708d762c
jsharo <jsharo@sudamericano.edu.ec>
2025-06-05 16:35:52 -0500

    Backend primer commit

---
+++ src/notifications/notifications-context.ts
@@ -0,0 +1,14 @@
+import { NotificationStrategy } from "./notification-strategy.interface";
+
+
+export class NotificationContext {
+  private strategy: NotificationStrategy;
+
+  setStrategy(strategy: NotificationStrategy) {
+    this.strategy = strategy;
+  }
+
+  executeStrategy(message: string) {
+    this.strategy.send(message);
+  }
+}
